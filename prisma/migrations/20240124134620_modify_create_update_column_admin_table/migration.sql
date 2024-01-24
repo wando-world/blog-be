0039e54de190a28388afba02c3582459e18ca820
wando <kdw40161217@gmail.com>
2024-01-25 01:48:45 +0900

    feat: db migrate sql 추가

---
+++ prisma/migrations/20240124134620_modify_create_update_column_admin_table/migration.sql
@@ -0,0 +1,10 @@
+/*
+  Warnings:
+
+  - You are about to alter the column `createdAt` on the `admin` table. The data in that column could be lost. The data in that column will be cast from `DateTime(3)` to `DateTime`.
+  - You are about to alter the column `updatedAt` on the `admin` table. The data in that column could be lost. The data in that column will be cast from `DateTime(3)` to `DateTime`.
+
+*/
+-- AlterTable
+ALTER TABLE `admin` MODIFY `createdAt` DATETIME NOT NULL DEFAULT NOW(),
+    MODIFY `updatedAt` DATETIME NOT NULL DEFAULT NOW() ON UPDATE NOW();
