ab11b140bd580b7d8a532b42746b33811aa2a077
wando <kdw40161217@gmail.com>
2024-01-27 17:14:01 +0900

    feat: prod, dev 환경 env 설정 #2

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AdminModule } from './admin/admin.module';
+import {ConfigModule} from "@nestjs/config";
+import * as process from "process";
 
 @Module({
-  imports: [AdminModule],
+  imports: [AdminModule, ConfigModule.forRoot({
+    cache: true,
+    isGlobal: true,
+    envFilePath: `.${process.env.NODE_ENV}.env`
+  })],
 })
 export class AppModule {}
