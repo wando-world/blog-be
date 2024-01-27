7a97a3d12fe88da76fbfef4999cd842a3cc28ade
wando <kdw40161217@gmail.com>
2024-01-27 15:58:51 +0900

    feat: swagger , prisma logging 설정 완료

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,9 @@ import { ResponseTransformInterceptor } from './common/interceptors/response-tra
 import { PrismaService } from './prisma/prisma.service';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: ['error', 'warn', 'log', 'debug', 'verbose'],
+  });
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -7,10 +7,11 @@ import {
 } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 import * as process from 'process';
+import { PrismaClientOptions } from '@prisma/client/runtime/library';
 
 @Injectable()
 export class PrismaService
-  extends PrismaClient
+  extends PrismaClient<PrismaClientOptions, 'info' | 'warn' | 'query' | 'error'>
   implements OnModuleInit, OnModuleDestroy
 {
   private readonly logger: Logger = new Logger(PrismaService.name);
@@ -23,33 +24,46 @@ export class PrismaService
           level: 'query',
         },
         {
-          emit: 'stdout',
+          emit: 'event',
           level: 'error',
         },
         {
-          emit: 'stdout',
+          emit: 'event',
           level: 'info',
         },
         {
-          emit: 'stdout',
+          emit: 'event',
           level: 'warn',
         },
       ],
     });
   }
 
-  async onModuleInit() {
+  async onModuleInit(): Promise<void> {
     this.logger.log('Prisma Init');
     await this.$connect();
+
+    this.$on('error', ({ message }) => {
+      this.logger.error(message);
+    });
+    this.$on('warn', ({ message }) => {
+      this.logger.warn(message);
+    });
+    this.$on('info', ({ message }) => {
+      this.logger.debug(message);
+    });
+    this.$on('query', ({ query, params }) => {
+      this.logger.log(`${query}; ${params}`);
+    });
   }
 
-  async onModuleDestroy() {
+  async onModuleDestroy(): Promise<void> {
     this.logger.log('Prisma Destroy');
     await this.$disconnect();
   }
 
-  async enableShutdownHooks(app: INestApplication) {
-    process.on('beforeExit', async () => {
+  async enableShutdownHooks(app: INestApplication): Promise<void> {
+    process.on('beforeExit', async (): Promise<void> => {
       await app.close();
     });
   }
